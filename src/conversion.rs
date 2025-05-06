4b4651f1e6637acdd52485b65a7f10502f125e2c
Tate <tate@cloudcannon.com>
2025-05-06 13:00:08 +1200

    Update saphyr + other crate

--- src/conversion.rs
+++ src/conversion.rs
@@ -1,30 +1,22 @@
-use saphyr::Yaml;
+use saphyr::{Scalar, Yaml};
 
 pub fn yaml_to_json(yaml: Yaml) -> Result<serde_json::Value, String> {
     match yaml {
-        Yaml::Real(x) => {
-            if let Ok(x) = x.parse::<serde_json::Number>() {
-                Ok(serde_json::Value::Number(x))
-            } else {
-                // Yaml supports some numbers that can't be represented in JSON e.g. .nan and .inf
-                // So we return a null if we encounter one of these.
-                Ok(serde_json::Value::Null)
-            }
-        }
-        Yaml::Integer(x) => Ok(serde_json::Value::from(x)),
-        Yaml::String(x) => Ok(serde_json::Value::from(x)),
-        Yaml::Boolean(x) => Ok(serde_json::Value::from(x)),
-        Yaml::Null => Ok(serde_json::Value::Null),
-        Yaml::Array(mut arr) => Ok(serde_json::Value::Array(
+        Yaml::Value(Scalar::FloatingPoint(x)) => Ok(serde_json::Value::from(x.into_inner())),
+        Yaml::Value(Scalar::Integer(x)) => Ok(serde_json::Value::from(x)),
+        Yaml::Value(Scalar::String(x)) => Ok(serde_json::Value::from(x)),
+        Yaml::Value(Scalar::Boolean(x)) => Ok(serde_json::Value::from(x)),
+        Yaml::Value(Scalar::Null) => Ok(serde_json::Value::Null),
+        Yaml::Sequence(mut arr) => Ok(serde_json::Value::Array(
             arr.drain(..).map(yaml_to_json).collect::<Result<_, _>>()?,
         )),
-        Yaml::Hash(mut hash) => Ok(serde_json::Value::Object(
+        Yaml::Mapping(mut hash) => Ok(serde_json::Value::Object(
             hash.drain()
                 .map(|(key, val)| {
                     let key = match key {
-                        Yaml::Integer(x) => Ok(x.to_string()),
-                        Yaml::String(x) => Ok(x),
-                        Yaml::Real(x) => Ok(x),
+                        Yaml::Value(Scalar::Integer(x)) => Ok(x.to_string()),
+                        Yaml::Value(Scalar::String(x)) => Ok(x.to_string()),
+                        Yaml::Value(Scalar::FloatingPoint(x)) => Ok(x.to_string()),
                         key => Err(format!("Bad key {key:?}")),
                     }?;
                     let val = yaml_to_json(val)?;
@@ -33,6 +25,9 @@ pub fn yaml_to_json(yaml: Yaml) -> Result<serde_json::Value, String> {
                 .collect::<Result<_, String>>()?,
         )),
         Yaml::BadValue => Err(format!("Bad value encountered when converting YAML")),
-        Yaml::Alias(_) => Err(format!("Unresolved alias encountered when converting YAML")), // TODO: Think about how to handle this
+        Yaml::Alias(_) => Err(format!("Unresolved alias encountered when converting YAML")),
+        Yaml::Representation(x, _, _) => Err(format!(
+            "Encountered unexpected representation when converting YAML: {x:?}"
+        )),
     }
 }
